0dd05fa2ba9bb7848f00b1118ec3c7edea978d00
Zokirjon-coder <zokirjonyetmishboyev@gmail.com>
2022-02-16 02:07:19 +0500

    :)

--- src/components/filialListItem/index.jsx
+++ src/components/filialListItem/index.jsx
@@ -1,54 +1,79 @@
-import React, { useState, useContext } from 'react';
-import { FilialItem, CircleBtn, Flex } from '../style/styled';
-import Trash from '../../assets/images/trash.png';
-import Edit from '../../assets/images/Path.png';
-import Mark from '../../assets/images/map-pin.png';
-import DrowerFilial from '../DrowerFilial';
-import { FilialsApi } from '../../context/FilialarContext';
-import { dataFilial } from '../../utils/dataFilial';
-import DrowerLocation from '../drowerLocation';
+import React, { useState, useContext } from "react";
+import { FilialItem, CircleBtn, Flex } from "../style/styled";
+import Trash from "../../assets/images/trash.png";
+import Edit from "../../assets/images/Path.png";
+import Mark from "../../assets/images/map-pin.png";
+import DrowerFilial from "../DrowerFilial";
+import { FilialsApi } from "../../context/FilialarContext";
+import { dataFilial } from "../../utils/dataFilial";
+import DrowerLocation from "../drowerLocation";
 
 const FilialListItem = (props) => {
-    const [showEditFilial, setShowEditFilial] = useState(false);
-    const [showLoc, setShowLoc] = useState(false);
-    const {id, filialnameuz, filialnameru, mark, worktime_start, worktime_end, location} = props.info;
-    const [filials, setFilials] = useContext(FilialsApi);
+  const [showEditFilial, setShowEditFilial] = useState(false);
+  const [showLoc, setShowLoc] = useState(false);
+  const {
+    id,
+    filialnameuz,
+    filialnameru,
+    mark,
+    worktime_start,
+    worktime_end,
+    location,
+  } = props.info;
+  const [filials, setFilials] = useContext(FilialsApi);
 
-    const onDelete = (id) => {
-        setFilials(
-            dataFilial.filter(filial => filial.id !== id)
-        );
-    };
-    
-    return (
-        <FilialItem>
-            <Flex>
-                <div className="filialnameuz">{filialnameuz}</div>
-                <div className="filialnameru">{filialnameru}</div>
-                <div className="mark">{mark}</div>
-                <div className="worktime">{worktime_start} - {worktime_end}</div>
-                <div className="fillialaction">
-                    <CircleBtn onClick={()=>setShowLoc(true)} param={{ diametr: "35", borderWidth: "4" }}>
-                        <Flex className="centered">
-                            <img style={{borderRadius: 0}} src={Mark} alt="" />
-                        </Flex>
-                    </CircleBtn>
-                    <CircleBtn onClick={()=>setShowEditFilial(true)} param={{ diametr: "35", borderWidth: "4" }}>
-                        <Flex className="centered">
-                            <img style={{borderRadius: 0}} src={Edit} alt="" />
-                        </Flex>
-                    </CircleBtn>
-                    <CircleBtn onClick={() => { onDelete(id) }} param={{ diametr: "35", borderWidth: "4" }}>
-                        <Flex className="centered">
-                            <img style={{borderRadius: 0}} src={Trash} alt="" />
-                        </Flex>
-                    </CircleBtn>
-                </div>
+  const onDelete = (id) => {
+    setFilials(filials.filter((filial) => filial.id !== id));
+  };
+
+  return (
+    <FilialItem>
+      <Flex>
+        <div className="filialnameuz">{filialnameuz}</div>
+        <div className="filialnameru">{filialnameru}</div>
+        <div className="mark">{mark}</div>
+        <div className="worktime">
+          {worktime_start} - {worktime_end}
+        </div>
+        <div className="fillialaction">
+          <CircleBtn
+            onClick={() => setShowLoc(true)}
+            param={{ diametr: "35", borderWidth: "4" }}
+          >
+            <Flex className="centered">
+              <img style={{ borderRadius: 0 }} src={Mark} alt="" />
             </Flex>
-            <DrowerLocation showLocation={[showLoc, setShowLoc]} location={location} />
-            <DrowerFilial input='edit' id={(id)} show={[showEditFilial, setShowEditFilial]} location={location} />
-        </FilialItem>
-    )
-}
+          </CircleBtn>
+          <CircleBtn
+            onClick={() => setShowEditFilial(true)}
+            param={{ diametr: "35", borderWidth: "4" }}
+          >
+            <Flex className="centered">
+              <img style={{ borderRadius: 0 }} src={Edit} alt="" />
+            </Flex>
+          </CircleBtn>
+          <CircleBtn
+            onClick={() => onDelete(id)}
+            param={{ diametr: "35", borderWidth: "4" }}
+          >
+            <Flex className="centered">
+              <img style={{ borderRadius: 0 }} src={Trash} alt="" />
+            </Flex>
+          </CircleBtn>
+        </div>
+      </Flex>
+      <DrowerLocation
+        showLocation={[showLoc, setShowLoc]}
+        location={location}
+      />
+      <DrowerFilial
+        input="edit"
+        id={id}
+        show={[showEditFilial, setShowEditFilial]}
+        location={location}
+      />
+    </FilialItem>
+  );
+};
 
 export default FilialListItem;
